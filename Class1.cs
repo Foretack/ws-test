3933f15bcc6e0e63119fa879c955f600ae909040
Foretack <observer.in.a.box@gmail.com>
2022-08-12 14:59:25 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.WebSockets;
+
+namespace ws_test;
+internal class Class1
+{
+    public Class1()
+    {
+        var ws = new ClientWebSocket();
+        var uri = new Uri("ws://0.0.0.0:8181");
+        ws.ConnectAsync(uri, new CancellationToken());
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Fleck;
+
+namespace ws_test;
+
+internal static class Program
+{
+    private static uint Id { get; set; } = 0;
+    static void Main(string[] args)
+    {
+        var server = new WebSocketServer("ws://0.0.0.0:7557");
+        server.RestartAfterListenError = true;
+        server.Start(socket =>
+        {
+            socket.OnOpen = () => Console.WriteLine("Open!");
+            socket.OnClose = () => Console.WriteLine("Closed!");
+            socket.OnMessage = message => socket.HandleMessage(message);
+        });
+
+        Console.ReadLine();
+    }
+
+    private static bool HandleMessage(this IWebSocketConnection con, string message)
+    {
+        Console.WriteLine($"Received: {message}");
+        con.Send($"Id: {Id}");
+        Id++;
+        return true;
+    }
+}
